758faaf84a5b39e543d3f590ef8b5bc826b27d8e
ath_t <ath_t@FORITOS>
2023-05-23 23:45:08 +0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		try
+		{
+			// simulate downloading a file to AppDataDirectory
+			using var stream = await FileSystem.OpenAppPackageFileAsync("my_file.png");
+			using var reader = new BinaryReader(stream);
+			{
+				byte[] buffer=new byte[5826];
+				reader.Read(buffer, 0, buffer.Length);
+				File.WriteAllBytes(Path.Combine(FileSystem.AppDataDirectory, "my_file.png"), buffer);
+			}
+
+			// open the file using Launcher.OpenAsync
+			// works on Android and Windows but not on IOS
+			await Launcher.OpenAsync(new OpenFileRequest("My File", new ReadOnlyFile(Path.Combine(FileSystem.AppDataDirectory, "my_file.png"))));
+		}
+		catch (Exception ex)
+		{
+		}
+	}
+}
+
